0c410299d62607ffee2a18b1e065497db952b7a6
Sannasichottu <sannasichottu@gmail.com>
2022-09-29 19:34:49 +0530

    empty

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ import Write from "./pages/write/Write";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 function App() {
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import SideBar from '../../components/sidebar/SideBar';
 import Posts from '../../components/posts/Posts';
 import "./home.css";
+import axios from "axios";
 
 export default function Home() {
+  const [posts,setPosts] = useState([]);
+  useEffect (() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts")
+      console.log(res)
+    }
+    fetchPosts()
+  },[])
   return (
       <>
       <Header />
